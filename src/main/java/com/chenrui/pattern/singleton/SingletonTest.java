3ed0dbeda60361ebe23572594532fce4b497a7f8
chenrui <chenrui0928@gmail.com>
2018-12-25 18:09:27 +0800

    单例测试

--- src/main/java/com/chenrui/pattern/singleton/SingletonTest.java
+++ src/main/java/com/chenrui/pattern/singleton/SingletonTest.java
@@ -2,6 +2,22 @@ package com.chenrui.pattern.singleton;
 
 public class SingletonTest {
 	public static void main(String[] args) {
+		/**
+		 * 双重检索测试
+		 */
+		Runnable task = new Runnable() {
+			public void run() {
+				DoubleCheckSingleton singleton = DoubleCheckSingleton.getInstance();
+				System.out.println(singleton);
+			}
+		};
+		int threadNum = 100;
+		long time = StartThread.startTaskInOnce(threadNum,task);
+		System.out.println("time = " + time);
+
+		/**
+		 * 饿汉式测试
+		 */
 
 	}
 }
